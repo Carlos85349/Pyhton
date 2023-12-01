23438a1066407f11f83cfe1e8a6fe42447d59d3a
Carlos85349 <147650880+Carlos85349@users.noreply.github.com>
2023-12-01 11:37:15 -0600

    Proyectos

---
+++ Tarea_Eddy/main.py
@@ -0,0 +1,9 @@
+from funciones import SistemaTransporteAerea  # Importa la clase SistemaTransporteAerea desde funciones.py
+
+def main():
+    sistema = SistemaTransporteAerea()  # Crea una instancia de la clase SistemaTransporteAerea
+
+    sistema.iniciar_sistema()  # Llama al método iniciar_sistema de la instancia sistema
+
+if __name__ == "__main__":
+    main()
