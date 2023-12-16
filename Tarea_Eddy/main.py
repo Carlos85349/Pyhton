0dbd66f1708252af8ed6effc4a40ddd7e687196f
Carlos85349 <147650880+Carlos85349@users.noreply.github.com>
2023-12-16 11:02:52 -0600

    Delete Tarea_Eddy directory

--- Tarea_Eddy/main.py
+++
@@ -1,9 +0,0 @@
-from funciones import SistemaTransporteAerea
-from interfaz_grafica import mostrar_interfaz_inicio_sesion
-
-def main():
-    sistema = SistemaTransporteAerea()
-    mostrar_interfaz_inicio_sesion(sistema)
-
-if __name__ == "__main__":
-    main()
